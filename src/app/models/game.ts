74fdf2b08ea8b427ff5186da58b755d39c18de2b
Raj Patel <rajp4690@gmail.com>
2018-03-23 00:34:43 -0400

    Some changes

--- src/app/models/game.ts
+++ src/app/models/game.ts
@@ -7,7 +7,10 @@ export class Game {
     ];
     Players:User[] = [];
     Picture:string = 'https://images6.alphacoders.com/863/thumb-1920-863542.png';
-    PlayedQuotes:Quote[] = [];
+    PlayedQuotes:Quote[] = [
+        new Quote('I know da way!', 'Raj', true),
+        new Quote('You wot mate xD', 'Diku', true)
+    ];
 }
 
 export class User {
@@ -18,4 +21,9 @@ export class Quote {
     Text:string;
     PlayerName:string;
     Chosen:boolean = false;
+    constructor(text:string, playerName:string, chosen:boolean) {
+        this.Text = text;
+        this.PlayerName = playerName;
+        this.Chosen = chosen;
+    }
 }
